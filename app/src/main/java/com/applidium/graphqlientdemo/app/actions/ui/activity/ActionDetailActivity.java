2e43de9c5455eba4cb942b553fc430b8a139b998
Kélian Clerc <kelian.clerc@applidium.com>
2017-07-26 17:28:34 +0200

    Wire to action detail and implement adapter

---
+++ app/src/main/java/com/applidium/graphqlientdemo/app/actions/ui/activity/ActionDetailActivity.java
@@ -0,0 +1,55 @@
+package com.applidium.graphqlientdemo.app.actions.ui.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.applidium.graphqlientdemo.R;
+import com.applidium.graphqlientdemo.app.actions.ui.adapter.ItemAdapter;
+import com.applidium.graphqlientdemo.app.common.BaseActivity;
+import com.applidium.graphqlientdemo.di.ComponentManager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.raizlabs.android.dbflow.config.FlowManager.getContext;
+
+public class ActionDetailActivity extends BaseActivity {
+
+    public static final String EXTRA_ACTION_ID = "EXTRA_ACTION_ID";
+    @BindView(R.id.recycler) RecyclerView recyclerView;
+    private ItemAdapter adapter;
+
+    public static Intent makeIntent(Context context, String actionId) {
+        Intent intent = new Intent(context, ActionDetailActivity.class);
+        intent.putExtra(EXTRA_ACTION_ID, actionId);
+        return intent;
+    }
+
+    @Override
+    protected void injectDependencies() {
+        ComponentManager.getLoggingComponent().inject(this);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupView();
+        setupAdapter();
+    }
+
+    private void setupAdapter() {
+        adapter = new ItemAdapter();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setupView() {
+        setContentView(R.layout.activity_action);
+        ButterKnife.bind(this);
+    }
+}
--- app/src/main/java/com/applidium/graphqlientdemo/app/profile/presenter/ProfilePresenter.java
+++ app/src/main/java/com/applidium/graphqlientdemo/app/profile/presenter/ProfilePresenter.java
@@ -5,6 +5,7 @@ import com.applidium.graphqlientdemo.app.actions.model.ActionViewModelMapper;
 import com.applidium.graphqlientdemo.app.common.Presenter;
 import com.applidium.graphqlientdemo.app.profile.model.ProfileViewModel;
 import com.applidium.graphqlientdemo.app.profile.model.ProfileViewModelBuilder;
+import com.applidium.graphqlientdemo.app.profile.navigator.ProfileNavigator;
 import com.applidium.graphqlientdemo.app.profile.ui.ProfileViewContract;
 import com.applidium.graphqlientdemo.core.interactor.actions.ActionResponse;
 import com.applidium.graphqlientdemo.core.interactor.actions.GetActionsInteractor;
@@ -24,17 +25,20 @@ public class ProfilePresenter extends Presenter<ProfileViewContract> implements 
     private final GetActionsInteractor actionsInteractor;
     private final ActionViewModelMapper actionMapper;
     private String userId;
+    private final ProfileNavigator navigator;
 
     @Inject ProfilePresenter(
         ProfileViewContract view,
         GetProfileInteractor interactor,
         GetActionsInteractor actionsInteractor,
+        ProfileNavigator navigator,
         ActionViewModelMapper actionMapper
     ) {
         super(view);
         this.interactor = interactor;
         this.actionsInteractor = actionsInteractor;
         this.actionMapper = actionMapper;
+        this.navigator = navigator;
     }
 
     @Trace
@@ -83,5 +87,6 @@ public class ProfilePresenter extends Presenter<ProfileViewContract> implements 
     }
 
     public void onAction(String id) {
+        navigator.navigateToActionDetail(id);
     }
 }
